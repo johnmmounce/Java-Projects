d22103210e12899467da3c5aa87fad6d23295859
John Mounce <johnnyboi@Johns-MBP.attlocal.net>
2023-02-04 23:25:55 -0600

    finished full credit and bonus

--- P03/bonus/Quiz.java
+++ P03/bonus/Quiz.java
@@ -31,6 +31,15 @@ public class Quiz{
             new Question("What does a constant declaration in java look like?",
                 new String[] {"const int num = 1;", "constant static final int num = 1", "private static final int num = 1;", "private int num = 1;"}, 
                 3),
+            new Question("What is used to build projects in this class, CSE1325?",
+                new String[] {"ANT", "MakeFile", "TNA", "Javac"}, 
+                1),
+            new Question("What type of software do you not need permission of any kind to use?",
+                new String[] {"Protective", "Trade Secret", "Shareware", "Public Domain"}, 
+                4),
+            new Question("Which is a primitive data type in java?",
+                new String[] {"boolean", "String", "arrays", "classes"}, 
+                1),
         };
     }
 }
