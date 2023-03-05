109e0067e845f3e15ba7c36c3463923c8ae5138b
John Mounce <johnnyboi@Johns-MBP.attlocal.net>
2023-03-05 16:29:09 -0600

    setting up P06

---
+++ P06/gui/nim/Elsa.java
@@ -0,0 +1,7 @@
+package store;
+
+public class Elsa {
+    public static void main(String[] args) {
+        new MainWin("ELSA");
+    }
+}
---
+++ P06/store/Option.java
@@ -0,0 +1,36 @@
+package store;
+
+public class Option {
+    protected String name;
+    protected long cost;
+
+    public Option(String name, long cost) {
+        this.name = name;
+        this.cost = cost;
+
+        if (cost < 0) {
+            throw new IllegalArgumentException("cost cannot be less than $0.00");
+        }
+    }
+
+    public long cost() {
+        return cost / 100;
+    }
+
+    public String toString() {
+        return name + " (" + cost + ") ";
+    }
+
+    public boolean equals(Object o) {
+        if (o == this) {
+            return true;
+        }
+
+        if (o instanceof Option) {
+            Option otherOption = (Option) o;
+            return otherOption.name.equals(this.name) &&
+                    otherOption.cost == this.cost;
+        }
+        return false;
+    }
+}
